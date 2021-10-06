ce24c4319041f9869a5ed1872b7b2bba1e94c27b
Victor Aparecido Hernandes Santos <regiotimao@gmail.com>
2021-10-06 01:59:03 -0300

    Padronização das mensagens de retorno

--- Catalog.Worker/Worker.cs
+++ Catalog.Worker/Worker.cs
@@ -55,8 +55,8 @@ namespace Catalog.Worker
                     $"{TratarMenssagem.RecuperarValor(mensagem, "Transacao")}"
                     );
 
-                //if (!string.IsNullOrEmpty(mensagem))
-                //    IniciaTransacao(mensagem);
+                if (!string.IsNullOrEmpty(mensagem))
+                    IniciaTransacao(mensagem);
 
                 mensagem = string.Empty;
                 _persistentConnection.DisposeMessage();
@@ -67,9 +67,48 @@ namespace Catalog.Worker
 
         public void IniciaTransacao(string mensagem)
         {
-            _logger.LogInformation("Inicio do fluxo de recebimento");
             DescobrirNome(mensagem);
-            _logger.LogInformation("Fim do fluxo de recebimento");
+        }
+
+        private void HasError(string msg, IResponse value)
+        {
+            if (!value.HasError())
+            {
+                _logger.LogInformation(
+                    $"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.fff")} " +
+                    $"[GTW_CATALOG -> GTW]   " +
+                    $"({TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")}): " +
+                    $"<result><status>000</status><message>Sucesso</message></result>"
+                    );
+                _persistentConnection.Send(_queue, false, false, false,
+                    $"<root>" +
+                        $"<numeroSolicitação>{TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")}</numeroSolicitação>" +
+                        $"<result>" +
+                            $"<status>000</status>" +
+                            $"<message>Sucesso</message>" +
+                        $"</result>" +
+                    $"</root>");
+            }
+            else
+            {
+                _logger.LogWarning($"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.fff")} " +
+                    $"[GTW_CATALOG -> GTW]   " +
+                    $"({TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")}): " +
+                    $"<result>" +
+                        $"<status>{value.Error().Code}</status>" +
+                            $"<message>{value.Error().Description}</message>" +
+                    $"</result>"
+                    );
+                _persistentConnection.Send(_queue, false, false, false,
+                    $"<root>" +
+                        $"<numeroSolicitação>{TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")}</numeroSolicitação>" +
+                        $"<result>" +
+                            $"<status>{value.Error().Code}</status>" +
+                            $"<message>{value.Error().Description}</message>" +
+                        $"</result>" +
+                    $"</root>");
+            }
+
         }
 
         public void DescobrirNome(string msg)
@@ -80,119 +119,54 @@ namespace Catalog.Worker
             switch (tipoTransacao.ToUpper())
             {
                 case "ADDBRAND":
-                    _logger.LogInformation("ADDBRAND");
                     _result = _serviceWorker.AddBrand(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("A nova marca foi adicionada com sucesso");
-                        _persistentConnection.Send(_queue, false, false, false, $"({TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")})  ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao adicionar uma marca");
-                        _persistentConnection.Send(_queue, false, false, false, $"({TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")})  erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "ADDITEM":
-                    _logger.LogInformation("ADDITEM");
                     _result = _serviceWorker.AddItem(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("Um novo produto foi adicionado no estoque");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao incluir um novo produto no estoque");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "ADDTYPE":
-                    _logger.LogInformation("ADDTYPE");
                     _result = _serviceWorker.AddType(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("O novo tipo foi adicionado com sucesso");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao adicionar um tipo");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "REGISTERITEM":
-                    _logger.LogInformation("REGISTERITEM");
                     _result = _serviceWorker.RegisterItem(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("Foi registrado um novo produto na base de dados");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro em registrar produto");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "REMOVEITEM":
-                    _logger.LogInformation("REMOVEITEM");
                     _result = _serviceWorker.RemoveItem(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("Produto removido com sucesso");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao remover um item");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "UPDATEBRAND":
-                    _logger.LogInformation("UPDATEBRAND");
                     _result = _serviceWorker.UpdateBrand(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("A  marca foi atualizada com sucesso");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao atualizar uma marca");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "UPDATEITEM":
-                    _logger.LogInformation("UPDATEITEM");
                     _result = _serviceWorker.UpdateItem(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("O produto foi atualizado com sucesso");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao atualizar um produto");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 case "UPDATETYPE":
-                    _logger.LogInformation("UPDATETYPE");
                     _result = _serviceWorker.UpdateType(msg);
-                    if (!_result.HasError())
-                    {
-                        _logger.LogInformation("O tipo de produto foi atualizado com sucesso");
-                        _persistentConnection.Send(_queue, false, false, false, "ok");
-                    }
-                    else
-                    {
-                        _logger.LogWarning("Erro ao atualizar um tipo de produto");
-                        _persistentConnection.Send(_queue, false, false, false, "erro");
-                    }
+                    HasError(msg, _result);
                     break;
                 default:
-                    _logger.LogWarning("tipoTransacao incorreta");
+                    _logger.LogWarning($"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.fff")} " +
+                    $"[GTW_CATALOG -> GTW]   " +
+                    $"({TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")}): " +
+                    $"<result>" +
+                        $"<status>999</status>" +
+                            $"<message>Tipo de transação incorreta</message>" +
+                    $"</result>"
+                    );
+                    _persistentConnection.Send(_queue, false, false, false,
+                        $"<root>" +
+                            $"<numeroSolicitação>{TratarMenssagem.RecuperarValor(msg, "numeroSolicitação")}</numeroSolicitação>" +
+                            $"<result>" +
+                                $"<status>999</status>" +
+                                    $"<message>Tipo de transação incorreta</message>" +
+                            $"</result>" +
+                        $"</root>");
                     break;
             }
         }
